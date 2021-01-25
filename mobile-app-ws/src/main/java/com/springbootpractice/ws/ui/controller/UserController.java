199f3f84d73944090eaa7905a7e096d5f90738f2
arafathbaig <arafath.moghul@gmail.com>
2021-01-26 00:44:28 +0530

    until validation

--- mobile-app-ws/src/main/java/com/springbootpractice/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/springbootpractice/ws/ui/controller/UserController.java
@@ -1,30 +1,37 @@
 package com.springbootpractice.ws.ui.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springbootpractice.ws.ui.model.request.UserDataRequestModel;
 import com.springbootpractice.ws.ui.model.response.UserClass;
 
 @RestController
 @RequestMapping("users")
 public class UserController {
 	
-	@GetMapping(path = "/{userId}")
-	public UserClass getUser(@PathVariable("userId") String id) {
+	@GetMapping(path = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<UserClass> getUser(@PathVariable("userId") String id) {
 		UserClass returnValue = new UserClass();
 		
 		returnValue.setEmail("araf@gmail.com");
 		returnValue.setFirstName("firstName");
 		returnValue.setLastName("lastName");
-		returnValue.setUserId("userId");
+		returnValue.setUserId(id);
 		
-		return returnValue;
+		return new ResponseEntity(HttpStatus.BAD_REQUEST);
 		
 		
 	}
@@ -37,9 +44,15 @@ public class UserController {
 	}
 	
 	
-	@PostMapping
-	public String createUser() {
-		return "create user was called";
+	@PostMapping(consumes = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE},
+			produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<UserClass> createUser(@Valid @RequestBody UserDataRequestModel userDetails) {
+		UserClass returnValue = new UserClass();
+		returnValue.setEmail(userDetails.getEmail());
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		
+		return new ResponseEntity(returnValue, HttpStatus.CREATED);
 	}
 	
 	@PutMapping
