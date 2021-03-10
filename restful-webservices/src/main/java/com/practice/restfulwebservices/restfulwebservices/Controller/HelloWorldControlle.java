6d4c06d5b847a3a36190df6d404849c83ed27d90
Arafath Baig <arafath.moghul@gmail.com>
2021-03-11 00:04:31 +0530

    internationalization

--- restful-webservices/src/main/java/com/practice/restfulwebservices/restfulwebservices/Controller/HelloWorldControlle.java
+++ restful-webservices/src/main/java/com/practice/restfulwebservices/restfulwebservices/Controller/HelloWorldControlle.java
@@ -1,13 +1,21 @@
 package com.practice.restfulwebservices.restfulwebservices.Controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.practice.restfulwebservices.restfulwebservices.Model.HelloWorldBean;
 
 @RestController
 public class HelloWorldControlle {
+	
+	@Autowired
+	private MessageSource messageSource;
 	
 	@GetMapping(path="/hello-world")
 	public HelloWorldBean helloWorldBean() {
@@ -17,6 +25,11 @@ public class HelloWorldControlle {
 	@GetMapping(path="/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+	@GetMapping(path= "/hello-world-inter")
+	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message",null, locale);
 	}
 	
 
