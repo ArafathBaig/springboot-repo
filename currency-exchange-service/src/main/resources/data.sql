0638b418e67c0c689da562f2af3724580b62ee9c
Arafath Baig <arafath.moghul@gmail.com>
2021-03-16 09:08:35 +0530

    adding jpa

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) values(20002,'USD','INR',65,'')
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) values(3456,'EUR','INR',85,'')
+insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment) values(8980,'AUD','INR',35,'')
